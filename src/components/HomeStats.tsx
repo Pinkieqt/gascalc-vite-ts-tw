8c8c7c3b8e80e99f579538a330375652c138e251
Dušan Kučeřík <izyman2@gmail.com>
2022-07-19 14:19:57 +0200

    Update HomeStats.tsx

--- src/components/HomeStats.tsx
+++ src/components/HomeStats.tsx
@@ -11,7 +11,7 @@ function HomeStats(data: PropTypes) {
   return (
     <div className="w-full">
       <div className="shadow-card rounded-xl w-full my-1 bg-teal-200 bg-opacity-10 dark:bg-opacity-20">
-        <div className="flex justify-around items-center py-2">
+        <div className="flex justify-around items-center pt-4 pb-2">
           <div className="text-center w-1/2 border-r border-slate-400">
             <div className="text-sm text-teal-500 dark:text-teal-200">
               průměrná spotřeba
