b675656a4f0dc627393e942f9ebd9435f300fbfc
Dušan Kučeřík <izyman2@gmail.com>
2022-07-19 09:15:54 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,6 +33,8 @@ function Home() {
       tmpPrice += el.price;
     });
     let tmpConsum = (tmpCons / (data.length)).toFixed(2)
+    console.log(tmpPrice)
+    console.log(data.length)
     setDashData({
       consumption: tmpConsum,
       price: (tmpPrice / data.length).toFixed(2),
