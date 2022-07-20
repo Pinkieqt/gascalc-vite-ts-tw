9db2b625807065438cf17046a43acd04efb03de4
Dušan Kučeřík <izyman2@gmail.com>
2022-07-20 16:27:15 +0200

    Update HomeStats.tsx

--- src/components/HomeStats.tsx
+++ src/components/HomeStats.tsx
@@ -12,9 +12,9 @@ interface PropTypes {
 function HomeStats(data: PropTypes) {
   return (
     <div className="w-full">
-      <div className="shadow-card rounded-xl w-full mt-5 mb-4 bg-teal-200 bg-opacity-10 dark:bg-opacity-20">
+      <div className="shadow-card rounded-xl w-full mt-5 mb-3 bg-teal-200 bg-opacity-10 dark:bg-opacity-20">
         <div className="flex justify-around items-center py-2">
-          <div className="text-center w-1/2 border-r border-slate-400">
+          <div className="text-center w-1/2 border-r border-teal-500 dark:border-teal-200">
             <div className="text-sm text-teal-500 dark:text-teal-200">
               průměrná spotřeba
             </div>
@@ -30,9 +30,9 @@ function HomeStats(data: PropTypes) {
           </div>
         </div>
       </div>
-      <div className="shadow-card rounded-xl w-full mb-4 bg-rose-200 bg-opacity-10 dark:bg-opacity-20">
+      <div className="shadow-card rounded-xl w-full mb-3 bg-rose-200 bg-opacity-10 dark:bg-opacity-20">
         <div className="flex justify-around items-center py-2">
-          <div className="text-center w-1/2 border-r border-slate-400">
+          <div className="text-center w-1/2 border-r border-rose-500 dark:border-rose-200">
             <div className="text-sm text-rose-500 dark:text-rose-200">
               průměrně natankováno
             </div>
@@ -50,15 +50,15 @@ function HomeStats(data: PropTypes) {
       </div>
       <div className="shadow-card rounded-xl w-full mb-10 bg-violet-200 bg-opacity-10 dark:bg-opacity-20">
         <div className="flex justify-around items-center py-2">
-          <div className="text-center w-1/2 border-r border-slate-400">
-            <div className="text-sm text-violet-500 dark:text-rose-200">
+          <div className="text-center w-1/2 border-r border-violet-500 dark:border-violet-200">
+            <div className="text-sm text-violet-500 dark:text-violet-200">
               celkem zaplaceno
             </div>
             <div className="text-3xl font-bold">{data.totalpaid}</div>
             <div className="text-xs text-zinc-400">czk</div>
           </div>
           <div className="text-center w-1/2">
-            <div className="text-sm text-violet-500 dark:text-rose-200">
+            <div className="text-sm text-violet-500 dark:text-violet-200">
               počet tankování
             </div>
             <div className="text-3xl font-bold">{data.totaltank}</div>
