0e813169d5e00f3e98e9c01d34c283f769218fdd
Dušan Kučeřík <izyman2@gmail.com>
2022-07-20 16:02:55 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,7 @@ function Home() {
       price: (tmpPrice / data.length).toFixed(2),
       totalpaid: (tmpPaid).toFixed(0),
       avgdist: (tmpDist / (data.length - zeroCounter)).toFixed(0),
-      avgliter: (tmpLiter).toFixed(2),
+      avgliter: (tmpLiter / data.length).toFixed(2),
       totaltank: (data.length).toFixed(0),
     });
   }
